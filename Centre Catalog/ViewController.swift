7d45f6dda6597942e187744d12b1e1846087aeba
macmini <macmini@littlelives.com>
2014-08-15 23:51:44 +0800

    minor update

--- Centre Catalog/ViewController.swift
+++ Centre Catalog/ViewController.swift
@@ -90,44 +90,7 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     }
     
     func readFromCode(){
-        centresData.append(Record(id: 1, name: "Brighton Montessori @ Fort",code:"CXZ124", unique_code:"cxz124"))
-        centresData.append(Record(id: 1, name: "Brighton Montessori @ Frankel",code:"ASX12E", unique_code:"asx12e"))
-        centresData.append(Record(id: 1, name: "Brighton Montessori @ River Valley",code:"QAW324", unique_code:"qaw324"))
-        centresData.append(Record(id: 1, name: "Brighton Montessori @ Sunset",code:"DCF214", unique_code:"dcf214"))
-        centresData.append(Record(id: 1, name: "Brighton Montessori @ Mountbatten",code:"ZASW21", unique_code:"qyqdk3"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Biopolis",code:"ASXWE2", unique_code:"asxwe2"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ NCS",code:"AQWS34", unique_code:"aqws34"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Nanyang Technological University",code:"RFDXC3", unique_code:"q5m7b3"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Vista Point",code:"XSDE21", unique_code:"xsde21"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Novena",code:"ASZQW2", unique_code:"aszqw2"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Changi Airport",code:"FGY4E2", unique_code:"fgy4e2"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Nanyang Polytechnic",code:"AQ1SW2", unique_code:"aq1sw2"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ KU Education Hub - The Grassroots' Club",code:"2WSDE3", unique_code:"twxnc7"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Icon",code:"3EDFR4", unique_code:"3edfr4"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Fusionopolis",code:"4RFGT5", unique_code:"4rfgt5"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ KK Hospital",code:"5TGHY6", unique_code:"5tghy6"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Tan Tock Seng Hospital",code:"3E4R5T", unique_code:"3e4r5t"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Sunshine Place (SSP)",code:"3E4RFD", unique_code:"3e4rfd"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Changi Business Park",code:"43ERFD", unique_code:"43erfd"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Alpha",code:"234REW", unique_code:"234rew"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Kent Ridge",code:"SDF234", unique_code:"sdf234"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Lakeside (LA)",code:"3E4FDC", unique_code:"3e4fdc"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Baby Haven (BH)",code:"3EDGT5", unique_code:"3edgt5"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Halifax Centre (HA)",code:"2WS3ED", unique_code:"2ws3ed"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Serangoon Centre (SE)",code:"12W98I", unique_code:"12w98i"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Aroozoo (AZ)",code:"265TR4", unique_code:"265tr4"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Katong Centre (KA)",code:"23WEXC", unique_code:"23wexc"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Ridgewood Centre (RW)",code:"2WS4RF", unique_code:"2ws4rf"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Siglap Centre (SI)",code:"1QA5TG", unique_code:"1qa5tg"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Tanglin (TA)",code:"43ERDF", unique_code:"43erdfg"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Sembawang (SW)",code:"234FD3", unique_code:"234fd3"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Jubilee Park (JP)",code:"17TYU4", unique_code:"17tyu4"))
-        centresData.append(Record(id: 1, name: "Pat's Schoolhouse @ Claymore Centre (CL)",code:"CVB45M", unique_code:"cvb45m"))
-        centresData.append(Record(id: 1, name: "Pat’s Schoolhouse @ Grassroots (GR)",code:"OS5D04", unique_code:"os5d04"))
-        centresData.append(Record(id: 1, name: "Learning Vision @ Paya Lebar",code:"OSTCK5", unique_code:"ostck5"))
-        centresData.append(Record(id: 1, name: "Brighton Montessori @ Grassroots Club",code:"KJRGH3", unique_code:"kjrgh3"))
-        centresData.append(Record(id: 1, name: "PCF Block 1", code: "AAATR", unique_code:"123456"))
-        searchResults = centresData
+        
     }
     
     func updateWeatherInfo() {
